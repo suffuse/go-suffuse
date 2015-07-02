e05722e44ce4409467c951afd1b6cffad09c77ce
Paul Phillips <paulp@improving.org>
2015-07-02 16:55:30 -0700

    Broke up path test somewhat.

--- suffuse/path_test.go
+++ suffuse/path_test.go
@@ -14,56 +14,72 @@ func Hours(n int) time.Duration {
   return NewDuration(Sprintf("%dh", n))
 }
 
-func (s *Tsfs) TestPathOsMethods(c *C) {
+/** TODO - run all path methods through fuse as well. */
+func (s *Tsfs) TestPathTimeManipulation(c *C) {
+  f := ScratchFile()
+  // Read the times on a scratch file.
+  atime0, mtime0 := f.OsStatAtimeMtime()
+  // Calculate recognizably different times from those.
+  atime1 := atime0.AddDate(-1, -2, -3)
+  mtime1 := mtime0.AddDate(-4, -5, -6)
+  // Set the scratch file times to the different ones.
+  f.OsChtimes(atime1, mtime1)
+  // Read the file times again.
+  atime2, mtime2 := f.OsStatAtimeMtime()
+  // And hope they look like the ones we set.
+  c.Assert(atime1, Equals, atime2)
+  c.Assert(mtime1, Equals, mtime2)
+}
+
+func (s *Tsfs) TestPathSymlinkLogic(c *C) {
   root := ScratchDir()
-  dir1 := root.Join("a/b/c")
+  d := root.Join("a/b/c")
 
+  // Create symlink "root/link1" -> "root/a/b/c"
   link1 := root.Join("link1")
-  link1.OsSymlink(dir1)
+  link1.OsSymlink(d)
 
+  // Create symlink "root/link2" -> "a/b/c"
   link2 := root.Join("link2")
-  link2.OsSymlink(dir1.Absolute())
+  link2.OsSymlink(NewPath("a/b/c"))
 
-  dir1.OsMkdirAll(NewFilePerms(0755))
-  dir1 = dir1.EvalSymlinks()
-  dir1.OsChdir()
+  // Create a/b/c, resolve all links, and chdir into it.
+  d.OsMkdirAll(NewFilePerms(0755))
+  d = d.EvalSymlinks()
+  d.OsChdir()
 
+  // Create symlink "link3" -> ".", where "." is now $d.
   link3 := NewPath("link3")
   link3.OsSymlink(DotPath)
 
-  dir2 := CwdNoLinks()
-  dir3 := NewPath(".").Absolute()
-  c.Assert(dir1, Equals, dir2)
-  c.Assert(dir1, Equals, dir3)
+  // Ensure that $d, $(pwd), and "." all resolve to the same path.
+  c.Assert(d, Equals, CwdNoLinks())
+  c.Assert(d, Equals, DotPath.Absolute())
 
+  // Ensure that link1, link2, and link3 all resolve to the same path.
   AssertSameFile(c, link1, link2)
   AssertSameFile(c, link1, link3)
-
-  file1 := root.Join("file1")
-  _, err := file1.OsCreate()
-  c.Assert(err, IsNil)
-
-  /** TODO - run these through fuse as well. */
-  atime, mtime := file1.OsStatAtimeMtime()
-  atime0 := atime.AddDate(-1, -2, -3)
-  mtime0 := mtime.AddDate(-4, -5, -6)
-  file1.OsChtimes(atime0, mtime0)
-
-  atime1, mtime1 := file1.OsStatAtimeMtime()
-  c.Assert(atime0, Equals, atime1)
-  c.Assert(mtime0, Equals, mtime1)
-
-  // OsChdir
-  // OsChmod
-  // OsChown
-  // OsChtimes
-  // OsMkdir
-  // OsMkdirAll
-  // OsRemoveAll
-  // OsRename
-  // OsLstat
-  // OsOpen
-  // OsOpenFile
-  // OsReadLink
-  // OsStat
 }
+
+func (s *Tsfs) TestPathFileCreation(c *C) {
+  root := ScratchDir()
+  f := root.Join("file")
+  _, err := f.OsCreate()
+  c.Assert(err, IsNil)
+}
+
+// Still to Test.
+//
+// OsChdir
+// OsChmod
+// OsChown
+// OsChtimes
+// OsMkdir
+// OsMkdirAll
+// OsRemoveAll
+// OsRename
+// OsLstat
+// OsOpen
+// OsOpenFile
+// OsReadLink
+// OsStat
