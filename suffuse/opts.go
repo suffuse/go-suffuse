44d717ce72390033dc20a704acd8f4daf604b685
EECOLOR <eecolor@qirx.org>
2015-07-04 02:37:39 +0200

    Manually creating a FlagSet in order to start suffuse more than once

--- suffuse/opts.go
+++ suffuse/opts.go
@@ -10,10 +10,11 @@ type SfsOpts struct {
   Config, Mountpoint, ExcludeRegex, VolName string
   Scratch, Verbose, Debug bool
   Args []string
+  fs *flag.FlagSet
 }
 
-func UsageAndExit() {
-  flag.Usage()
+func (opts SfsOpts) UsageAndExit() {
+  opts.fs.Usage()
   os.Exit(2)
 }
 
@@ -39,7 +40,7 @@ func (opts SfsOpts) Create() (Sfs, error) {
 
   // Exactly one incoming path for the moment.
   // Eventually more than one could mean a union mount.
-  if len(opts.Args) != 1 { UsageAndExit() }
+  if len(opts.Args) != 1 { opts.UsageAndExit() }
 
   mfs := Sfs {
     Mountpoint : mnt,
@@ -63,25 +64,26 @@ func (opts SfsOpts) GetMountpoint() Path {
   switch {
     case opts.Scratch          : return ScratchDir()
     case opts.Mountpoint != "" : return NewPath(opts.Mountpoint)
-    default                    : UsageAndExit() ; return Path{}
+    default                    : opts.UsageAndExit() ; return Path{}
   }
 }
 
 func ParseSfsOpts() SfsOpts {
   opts := SfsOpts{}
 
-  // flag.StringVar(&opts.Config, "c", "", "suffuse config file")
-  // flag.StringVar(&opts.ExcludeRegex, "x", "", "name exclusion regex")
-  flag.StringVar(&opts.Mountpoint, "m", "", "mount point")
-  flag.StringVar(&opts.VolName, "n", "", "volume name (OSX only)")
-  flag.BoolVar(&opts.Scratch, "t", false, "create scratch directory as mount point")
-  flag.BoolVar(&opts.Verbose, "v", false, "log at INFO level")
-  flag.BoolVar(&opts.Debug, "d", false, "log at DEBUG level")
-  flag.Usage = func () {
+  fs := flag.NewFlagSet("suffuse", flag.ExitOnError)
+  // fs.StringVar(&opts.Config, "c", "", "suffuse config file")
+  // fs.StringVar(&opts.ExcludeRegex, "x", "", "name exclusion regex")
+  fs.StringVar(&opts.Mountpoint, "m", "", "mount point")
+  fs.StringVar(&opts.VolName, "n", "", "volume name (OSX only)")
+  fs.BoolVar(&opts.Scratch, "t", false, "create scratch directory as mount point")
+  fs.BoolVar(&opts.Verbose, "v", false, "log at INFO level")
+  fs.BoolVar(&opts.Debug, "d", false, "log at DEBUG level")
+  fs.Usage = func () {
     Echoerr("Usage: %s <options> [path path ...]\n", os.Args[0])
-    flag.PrintDefaults()
+    fs.PrintDefaults()
   }
-  flag.Parse()
-  opts.Args = flag.Args()
+  fs.Parse(os.Args[1:])
+  opts.Args = fs.Args()
   return opts
 }
