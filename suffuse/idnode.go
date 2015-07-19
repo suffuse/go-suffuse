fa64043a5b306fd78c43064d669404a6710d9ce0
EECOLOR <eecolor@qirx.org>
2015-07-18 18:02:38 -0600

    Removal of unused functions, note that this commit is tagged because the removed code has research value

--- suffuse/idnode.go
+++ suffuse/idnode.go
@@ -1,9 +1,6 @@
 package suffuse
 
 import (
-  "github.com/suffuse/go-suffuse/suffuse/xattr"
-  "os"
-  "time"
   "golang.org/x/net/context"
   "bazil.org/fuse/fs"
   f "bazil.org/fuse"
@@ -23,102 +20,6 @@ func NewIdNode(path Path) *IdNode {
 }
 
 func (x *IdNode) String() string { return string(x.Path) }
-
-// Access checks whether the calling context has permission for
-// the given operations on the receiver. If so, Access should
-// return nil. If not, Access should return EPERM.
-//
-// Note that this call affects the result of the access(2) system
-// call but not the open(2) system call. If Access is not
-// implemented, the Node behaves as if it always returns nil
-// (permission granted), relying on checks in Open instead.
-// TODO - permissions check.
-func (x *IdNode) Access(ctx context.Context, req *f.AccessRequest) error {
-  trace("[%v] Access", x.Path)
-  return nil
-}
-
-// There doesn't appear to be any way to set one or the other
-// time, you always have to specify two values. So if both are
-// valid according to fuse, we pass both, but if only one is,
-// we stat the current values and pass what was already there
-// for the not-being-set-right-now slot.
-func setattrChtimes(path Path, req *f.SetattrRequest) error {
-  isAtime := req.Valid.Atime()
-  isMtime := req.Valid.Mtime()
-
-  if isAtime || isMtime {
-    var atime, mtime time.Time
-    if isAtime && isMtime {
-      atime = req.Atime
-      mtime = req.Mtime
-    } else {
-      // one of them needs to be preserved
-      atime, mtime = path.OsStatAtimeMtime()
-      if isAtime { atime = req.Atime }
-      if isMtime { mtime = req.Mtime }
-    }
-    return path.OsChtimes(atime, mtime)
-  }
-  return nil
-}
-
-// man 2 chown:
-//   One of the owner or group id's may be left unchanged by specifying it as -1.
-func setattrChown(path Path, req *f.SetattrRequest) error {
-  isValidUid := req.Valid.Uid()
-  isValidGid := req.Valid.Gid()
-
-  if isValidUid || isValidGid {
-    uid, gid := -1, -1
-    if isValidUid { uid = int(req.Uid) }
-    if isValidGid { gid = int(req.Gid) }
-    return path.OsChown(uid, gid)
-  }
-  return nil
-}
-
-// TODO - identify which bits besides permission bits can
-// theoretically be set, as opposed to being unmodifiable
-// consequences of the inode.
-func setattrChmod(path Path, req *f.SetattrRequest) error {
-  if req.Valid.Mode() {
-    return path.OsChmod(req.Mode & os.ModePerm)
-  }
-  return nil
-}
-
-// "Truncate" is kind of a scary name, but it's used to both shrink
-// and increase the size of files.
-func setattrTruncate(path Path, req *f.SetattrRequest) error {
-  if req.Valid.Size() {
-    return path.OsTruncate(int64(req.Size))
-  }
-  return nil
-}
-
-// Setattr sets the standard metadata for the receiver.
-//
-// Note, this is also used to communicate changes in the size of
-// the file. Not implementing Setattr causes writes to be unable
-// to grow the file (except with OpenDirectIO, which bypasses that
-// mechanism).
-//
-// req.Valid is a bitmask of what fields are actually being set.
-// For example, the method should not change the mode of the file
-// unless req.Valid.Mode() is true.
-func (x *IdNode) Setattr(ctx context.Context, req *f.SetattrRequest, resp *f.SetattrResponse) error {
-  trace("Setattr(%v, %+v)", x.Path, *req)
-  // Not Yet Implemented:
-  // req.Valid.{ Handle, LockOwner, Crtime, Chgtime, Bkuptime, Flags }
-
-  return FindError(
-    setattrChtimes(x.Path, req),
-    setattrChown(x.Path, req),
-    setattrChmod(x.Path, req),
-    setattrTruncate(x.Path, req),
-  )
-}
 
 func (x *IdNode) Attr(ctx context.Context, attr *f.Attr) error {
   trace("[%v] Attr", x.Path)
@@ -191,49 +92,4 @@ func (x *IdNode) ReadAll(ctx context.Context) ([]byte, error) {
     }
   }
   return nil, NotExist()
-}
-
-/** Write ops.
- */
-
-func (x *IdNode) Mkdir(ctx context.Context, req *f.MkdirRequest) (fs.Node, error) {
-  trace("[%v] Mkdir(%+v)", x.Path, *req)
-
-  name := req.Name
-  mode := req.Mode
-  umask := req.Umask
-  path := x.Path.Join(name)
-  err := path.OsMkdir(mode & umask)
-
-  MaybeLog(err)
-
-  if err != nil { return nil, err }
-  return NewIdNode(path), nil
-}
-func (x *IdNode) Create(ctx context.Context, req *f.CreateRequest, resp *f.CreateResponse) (fs.Node, fs.Handle, error) {
-  trace("[%v] Create(%+v)", x.Path, *req)
-  return nil, nil, NotSupported()
-}
-
-/** Xattr ops.
- */
-func (x *IdNode) Getxattr(ctx context.Context, req *f.GetxattrRequest, resp *f.GetxattrResponse) error {
-  trace("[%v] Getxattr(%v)", x.Path, req.Name)
-  bytes := xattr.Get(string(x.Path), req.Name)
-  if bytes != nil { resp.Xattr = bytes }
-  return nil
-}
-func (x *IdNode) Listxattr(ctx context.Context, req *f.ListxattrRequest, resp *f.ListxattrResponse) error {
-  trace("[%v] Listxattr", x.Path)
-  names := xattr.List(string(x.Path))
-  if names != nil { resp.Append(names...) }
-  return nil
-}
-func (x *IdNode) Setxattr(ctx context.Context, req *f.SetxattrRequest) error {
-  trace("[%v] Getxattr(%v, %v)", x.Path, req.Name, string(req.Xattr))
-  return xattr.Set(string(x.Path), req.Name, req.Xattr)
-}
-func (x *IdNode) Removexattr(ctx context.Context, req *f.RemovexattrRequest) error {
-  trace("[%v] Removexattr(%v)", x.Path, req.Name)
-  return xattr.Remove(string(x.Path), req.Name)
 }
