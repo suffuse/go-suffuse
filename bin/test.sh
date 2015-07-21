6eeb6908628f21327741b39fa8710e37f17cdeae
Paul Phillips <paulp@improving.org>
2015-07-21 09:26:36 -0700

    Use gox for crossbuilding.

--- bin/test.sh
+++ bin/test.sh
@@ -3,13 +3,24 @@
 
 clear && printf '\e[3J' && clear
 
-function explain {
-  echo "                  ^"
-  echo "RUN THIS TO FIX: cd \$(go env GOROOT)/src ; GOOS=$1 GOARCH=amd64 ./make.bash --no-clean 2>&1"
-  echo ""
+# Eventually we'll want a windows build but that may be a long way off.
+# Note that one can "cross-download" windows dependencies with go get
+# by setting GOOS, for example:
+#
+#   GOOS=windows go get github.com/shirou/w32
+#
+OSARCH="darwin/amd64 linux/amd64"
+
+# I don't see any way in gox to determine if the toolchain has already
+# been built. Super annoying. It will rebuild it if one passes -build-toolchain
+# when it's already built, so right now this depends on the toolchains being
+# built at the same time gox is downloaded.
+which gox >/dev/null || {
+  echo "go getting gox for cross-build..."
+  go get github.com/mitchellh/gox
+  echo "Building gox toolchains..."
+  gox -osarch="$OSARCH" -build-toolchain
 }
 
-GOOS=darwin GOARCH=amd64 go build ./...; if [ $? -eq 1 ]; then explain 'darwin'; fi
-GOOS=linux  GOARCH=amd64 go build ./...; if [ $? -eq 1 ]; then explain 'linux'; fi
-
+gox -osarch="$OSARCH" -output "$TMPDIR/{{.Dir}}_{{.OS}}_{{.Arch}}" ./...
 go test -v ./...
